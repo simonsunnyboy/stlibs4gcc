b30d76b3c7a4524bf38061808de1d16b2535468e
marndt@final-memory.org <marndt@final-memory.org@77d0f9ee-98b1-3bea-9cd1-bb6a18935fa8>
2014-10-12 10:44:06 +0000

    - added testprogram and libarry for SNDH replay

---
+++ playsndh/sndhtest.c
@@ -0,0 +1,48 @@
+/* Test program for SNDH routines */
+
+#include <stdint.h>
+#include <osbind.h>
+#include "sndh.h"
+
+uint8_t ramdisk[20000];
+
+static void PRINT(const char *str1, const char *str2)
+{
+    (void)Cconws(str1);
+    (void)Cconws(str2);
+    (void)Cconws("\r\n");
+    return;
+}
+
+int main(void)
+{
+	SNDHTune mytune;
+	void* tuneptr;
+
+	tuneptr = &ramdisk[0];
+	if(tuneptr != 0)
+	{
+	    long handle;
+
+  	    handle = Fopen ( "heroques.snd", 0 );
+        Fseek ( 0,handle, 0 );
+        Fread ( handle, 20000, tuneptr);
+        Fclose ( handle );
+
+		SNDH_GetTuneInfo(tuneptr,&mytune);
+
+		SNDH_PlayTune(&mytune,0);
+
+
+		PRINT("TITL: ",mytune.title);
+		PRINT("COMP: ",mytune.composer);
+		PRINT("RIPP: ",mytune.ripper);
+		PRINT("CONV: ",mytune.conv);
+		/*PRINT("FREQ: ",mytune.freq); */
+
+		Cconin();
+		SNDH_StopTune();
+	}
+
+	return 0;
+}
